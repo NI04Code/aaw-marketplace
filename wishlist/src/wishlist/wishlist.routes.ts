eba4f220131f4505996e9767a23922ed317e796e
NI04Code <naufalichsan114@gmail.com>
2025-03-28 05:45:29 +0700

    a03: make changes to wishlist microservices

--- wishlist/src/wishlist/wishlist.routes.ts
+++ wishlist/src/wishlist/wishlist.routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', verifyJWT, Handler.getAllUserWishlistHandler);
 router.get('/:id', verifyJWT, validate(Validation.getWishlistByIdSchema), Handler.getWishlistByIdHandler);
-router.post('/', verifyJWT, validate(Validation.createWishlistSchema), Handler.createWishlistHandler);
+router.post('/v1', verifyJWT, validate(Validation.createWishlistSchema), Handler.createWishlistHandler);
 router.put('/:id', verifyJWT, validate(Validation.updateWishlistSchema), Handler.updateWishlistHandler);
 router.delete('/remove', verifyJWT, validate(Validation.removeProductFromWishlistSchema), Handler.removeProductFromWishlistHandler);
 router.delete('/:id', verifyJWT, validate(Validation.deleteWishlistSchema), Handler.deleteWishlistHandler);
