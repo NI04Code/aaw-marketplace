7ca2f240383f0b53dcbcad62f69e6cceb653011e
NI04Code <naufalichsan114@gmail.com>
2025-05-08 14:29:55 +0700

    add timestamp in logger

--- authentication/src/utils/logger.ts
+++ authentication/src/utils/logger.ts
@@ -1,7 +1,14 @@
-// @src/utils/logger.ts
+function formatLog(level: string, message: string, meta?: any) {
+    const timestamp = new Date().toISOString();
+    const base = `[${timestamp}] [${level.toUpperCase()}] ${message}`;
+    return meta ? `${base} ${JSON.stringify(meta)}` : base;
+}
+
 export const logger = {
-    info: (message: string, meta?: any) => console.info(message, meta || ''),
-    error: (message: string, meta?: any) => console.error(message, meta || ''),
-    warn: (message: string, meta?: any) => console.warn(message, meta || ''),
+    info: (message: string, meta?: any) =>
+        console.info(formatLog('info', message, meta)),
+    error: (message: string, meta?: any) =>
+        console.error(formatLog('error', message, meta)),
+    warn: (message: string, meta?: any) =>
+        console.warn(formatLog('warn', message, meta)),
 };
-  
