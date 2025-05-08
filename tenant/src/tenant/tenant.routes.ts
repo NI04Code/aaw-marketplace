2179abcc101dccfd7d2392a9c905d3b26194f20c
NI04Code <naufalichsan114@gmail.com>
2025-05-08 14:18:37 +0700

    add api versioning in tenant

--- tenant/src/tenant/tenant.routes.ts
+++ tenant/src/tenant/tenant.routes.ts
@@ -6,8 +6,8 @@ import * as Handler from './tenant.handler';
 const router = express.Router();
 
 router.get('/v1/:tenant_id', verifyJWTTenant, validate(Validation.getTenantSchema), Handler.getTenantHandler);
-router.post('', verifyJWTTenant, validate(Validation.createTenantSchema), Handler.createTenantHandler);
-router.put('/:old_tenant_id', verifyJWTTenant, validate(Validation.editTenantSchema), Handler.editTenantHandler);
-router.delete('', verifyJWTTenant, validate(Validation.deleteTenantSchema), Handler.deleteTenantHandler);
+router.post('/v1', verifyJWTTenant, validate(Validation.createTenantSchema), Handler.createTenantHandler);
+router.put('/v1/:old_tenant_id', verifyJWTTenant, validate(Validation.editTenantSchema), Handler.editTenantHandler);
+router.delete('/v1', verifyJWTTenant, validate(Validation.deleteTenantSchema), Handler.deleteTenantHandler);
 
 export default router;
