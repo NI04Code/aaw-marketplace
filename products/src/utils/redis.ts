33d9d1921debb188cb12e2f4fa28afa5069d5a8e
NI04Code <naufalichsan114@gmail.com>
2025-05-20 14:32:46 +0700

    change redis.url

--- products/src/utils/redis.ts
+++ products/src/utils/redis.ts
@@ -2,11 +2,8 @@ import Redis from "ioredis";
 
 const REDIS_URL = process.env.REDIS_URL ?? "redis://:osbanas2025redis@localhost:6379";
 
-export const redis = new Redis.Cluster(
-    [{ host: `${REDIS_URL}`, port: 6379 }],
-    {
-      dnsLookup: (address, callback) => callback(null, address),
-      redisOptions: {
-        tls: {},
-      },
-    });
+export const redis = new Redis({
+    host: REDIS_URL,
+    port: 6379,
+    tls: {}, // Required for AWS Serverless Redis
+  });
