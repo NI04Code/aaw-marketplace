6948a67bdfc846eeae0b20eabb3ca825ee3b2bff
NI04Code <naufalichsan114@gmail.com>
2025-05-29 15:09:03 +0700

    add tls

--- products/src/utils/redis.ts
+++ products/src/utils/redis.ts
@@ -2,4 +2,8 @@ import Redis from "ioredis";
 
 const REDIS_URL = process.env.REDIS_URL ?? "redis://:osbanas2025redis@localhost:6379";
 
-export const redis = new Redis(REDIS_URL);
+export const redis =new Redis({
+  host: REDIS_URL,
+  port: 6379,
+  tls: {}
+});
